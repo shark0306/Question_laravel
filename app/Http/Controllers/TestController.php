f69887f5b53ab139d2e17228c0847e0e381b3c0b
shark36 <mgame36nseam@icloud.com>
2021-05-19 09:58:33 +0900

    first-commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return "建物です";
+    }
+    public function end($room)
+    {
+        return '部屋番号は' . $room . 'です';
+    }
+}
